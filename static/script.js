3d6436824a8e3a160bab9919b37f54c797ad9f99
tkaracam <tolgahankaracam@outlook.de>
2026-03-06 14:50:44 +0100

    Make fog-out fade away and match defog speed

--- static/script.js
+++ static/script.js
@@ -360,7 +360,7 @@ function initHomeHeroSlider() {
     return;
   }
 
-  const FOG_OUT_MS = 1300;
+  const FOG_OUT_MS = 2400;
   const FOG_IN_MS = 2400;
   const AUTO_MS = 6000;
   let currentIndex = slides.findIndex((slide) => slide.classList.contains("is-active"));
